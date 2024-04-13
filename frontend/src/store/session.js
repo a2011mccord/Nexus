c2488b3ca0a41806b28e3dff42a4ae73f8853efc
Andrew McCord <a2011mccord@gmail.com>
2024-04-13 14:35:04 -0400

    added restore session user thunk action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -12,7 +12,7 @@ const removeUser = () => ({
   type: REMOVE_USER
 });
 
-export const login = user => async (dispatch) => {
+export const login = user => async dispatch => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
     method: 'POST',
@@ -25,6 +25,13 @@ export const login = user => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
+
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+}
 
 const initialState = { user: null };
 
