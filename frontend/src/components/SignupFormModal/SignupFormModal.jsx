ae28c2dc6e6bfafa2978c988187f095f5b9651a8
Andrew McCord <a2011mccord@gmail.com>
2024-04-13 17:09:16 -0400

    converted signup form page to signup form modal

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
 import * as sessionActions from '../../store/session';
 import './SignupForm.css'
 
-function SignupFormPage() {
+function SignupFormModal() {
   const dispatch = useDispatch();
-  const sessionUser = useSelector(state => state.session.user);
+  const { closeModal } = useModal();
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -14,8 +14,6 @@ function SignupFormPage() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [errors, setErrors] = useState({});
-
-  if (sessionUser) return <Navigate to='/' replace={true} />;
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -30,12 +28,14 @@ function SignupFormPage() {
           lastName,
           password
         })
-      ).catch(async res => {
-        const data = await res.json();
-        if (data?.errors) {
-          setErrors(data.errors);
-        }
-      });
+      )
+        .then(closeModal)
+        .catch(async res => {
+          const data = await res.json();
+          if (data?.errors) {
+            setErrors(data.errors);
+          }
+        });
     }
     return setErrors({
       confirmPassword: "Confirm Password field must be the same as the Password field"
@@ -112,4 +112,4 @@ function SignupFormPage() {
   );
 }
 
-export default SignupFormPage;
+export default SignupFormModal;
