f5c30865e9d5a17b9d142c2cc1aca583ec12af07
Andrew McCord <a2011mccord@gmail.com>
2024-04-24 09:28:34 -0400

    set up landing page baseline

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,17 +1,79 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import ProfileButton from '../Navigation/ProfileButton';
+import OpenModalMenuItem from "../OpenModalMenuItem";
+import SignupFormModal from '../SignupFormModal';
 import './LandingPage.css';
 
 function LandingPage() {
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user)
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
   if (sessionUser) return <Navigate to="/home" replace={true} />;
 
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setErrors({});
+    return dispatch(sessionActions.login({ credential, password }))
+      .then(closeModal)
+      .catch(
+        async res => {
+          const data = await res.json();
+          if (data?.errors) setErrors(data.errors);
+        }
+      );
+  };
+
+  const demoUser = () => {
+    setCredential('Demo-lition');
+    setPassword('password');
+  };
+
   return (
     <div className='landing-cont'>
-      <h1>Log In</h1>
-      <ProfileButton user={sessionUser} />
+      <h1>Welcome to Nexus</h1>
+      <div className='form-cont'>
+        <h2>Log In</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username or Email
+            <input
+              type="text"
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+          </label>
+          {errors.credential && <p>{errors.credential}</p>}
+
+          <label>
+            Password
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          {errors.credential && <p>{errors.password}</p>}
+
+          <div className="buttons">
+            <button type="submit">Log In</button>
+            <button onClick={demoUser}>Demo User</button>
+          </div>
+        </form>
+      </div>
+      <div className='signup'>
+        Don't have an account?
+        <OpenModalMenuItem
+          itemText="Sign Up"
+          modalComponent={<SignupFormModal />}
+        />
+      </div>
     </div>
   )
 }
